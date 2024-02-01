bfd354140a1318e160ce9fac46b982f47b0eeae2
Aditya <adityarpadekar@gmail.com>
2024-02-02 02:15:16 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import { Navbar } from "./components/Navbar";
 function App() {
     return (
         <>
-            <div className="w-full h-screen flex justify-center bg-gradient-to-b from-cyan-200 to-blue-100">
+            <div className="w-full min-h-screen flex justify-center bg-gradient-to-b from-cyan-200 to-blue-100">
                 <div className="flex flex-col items-center w-full lg:w-[1150px]">
                     <Navbar />
                     <Content />
